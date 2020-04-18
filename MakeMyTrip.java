12921a9cf528d6437bcc7eb6cb31944287a5bd86
GunalanK <63818168+GunalanK@users.noreply.github.com>
2020-04-18 20:55:04 +0530

    Update MakeMyTrip.java

--- MakeMyTrip.java
+++ MakeMyTrip.java
@@ -12,9 +12,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MakeMyTrip {
 
 	public static void main(String[] args) throws InterruptedException {
-					//Launch the URL
-					System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-					ChromeDriver driver=new ChromeDriver();
+				//Launch the URL
+				System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+				ChromeDriver driver=new ChromeDriver();
 			        driver.get("http://www.makemytrip.com/");
 			        
 			        //Maxmize the window
